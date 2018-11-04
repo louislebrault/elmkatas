0dd3c216aa51700e6f3c9343ef2ba93273f6ff18
Louis Lebrault <louis.lebrault@gmail.com>
2018-11-04 14:53:37 +0100

    4th case implemented

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,21 @@
 module Main exposing (..)
 
+zeroOr = Maybe.withDefault 0
+
+normalizeOperators : String -> String
+normalizeOperators = String.replace "\n" ","
+
+mapStringToInt : String -> Int
+mapStringToInt x = zeroOr (String.toInt x)
+
+splitOnComma = String.split(",")
+
 calculateString : String -> Int
 calculateString str =
   if str == "" then 0
-  else if String.length str == 1 then Maybe.withDefault 0 (String.toInt str)
+  else if String.length str == 1 then zeroOr (String.toInt str)
   else List.foldr
   (\x a -> x + a)
   0
-  (List.map (\x -> Maybe.withDefault 0 (String.toInt x)) (String.split "," str))
+  (List.map mapStringToInt (splitOnComma (normalizeOperators str)))
 
-splitStringOnComma : String -> List String
-splitStringOnComma str = String.split "," str
