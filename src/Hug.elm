04ebc664aae00f2a1899e3a5190c27633a084acf
Louis Lebrault <louis.lebrault@gmail.com>
2018-11-19 01:48:50 +0100

    WIP trying to listen to keyboard events

--- src/Hug.elm
+++ src/Hug.elm
@@ -1,11 +1,15 @@
 import Array
 import Random
 import Browser
+import Browser.Events as Events
+import Json.Decode as Decode
+import Platform.Sub as Sub
 import Html exposing (Html, button, div, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
 
--- Next : Do something with subscriptions
+-- Next : A chaque fois qu'on appuie sur espace, changer la couleur de la page
+-- Comment definir quelle touche a été pressée : https://github.com/elm/browser/blob/1.0.0/notes/keyboard.md
 
 -- MAIN
 main = Browser.element
@@ -32,6 +36,7 @@ init _ =
 type Msg
   = Dance
   | RandomIndex Int
+  | KeyMsg
 
 messageList = Array.fromList ["cc", "tavu", "sposé", "Calin.", "booh"]
 
@@ -41,22 +46,40 @@ getMessageByIndex idx = Maybe.withDefault "Moga" (Array.get idx messageList)
 generateRandomInt : Cmd Msg
 generateRandomInt = Random.generate RandomIndex (Random.int 0 (Array.length messageList))
 
+type Key
+  = Character Char
+  | Control String
+
+keyDecoder : Decode.Decoder Msg
+keyDecoder =
+  Decode.map toKey (Decode.field "key" Decode.string)
+
+toKey : String -> Key
+toKey string =
+  case String.uncons string of
+    Just (char, "") ->
+      Character char
+
+    _ ->
+      Control string
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Dance ->
       if model.kirby == "(>'.')>"
-      then ( {kirby = "<('.'<)", message = model.message }, generateRandomInt)
-      else ( {kirby = "(>'.')>", message = model.message }, generateRandomInt)
+      then ( { kirby = "<('.'<)", message = model.message }, generateRandomInt)
+      else ( { kirby = "(>'.')>", message = model.message }, generateRandomInt)
 
     RandomIndex idx ->
-      ( {kirby = model.kirby, message = getMessageByIndex idx }, Cmd.none)
+      ( { kirby = model.kirby, message = getMessageByIndex idx }, Cmd.none)
 
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+  Sub.batch
+    [ Events.onKeyDown keyDecoder ]
 
 -- VIEW
 
